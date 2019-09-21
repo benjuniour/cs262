ac28aabad0c7c6b4877e37e94cd276b979b22081
benjuniour <benjuniour@users.noreply.github.com>
2019-09-20 22:46:17 -0400

    personalized team project

--- Lab03/src/app/home/home.component.ts
+++ Lab03/src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "my-app",
   template: `
-    <ActionBar title="SnapPong" class="action-bar"></ActionBar>
+    <ActionBar title="Bernard_HelloWorld" class="action-bar"></ActionBar>
     <Image src="https://pngimg.com/uploads/snapchat/snapchat_PNG61.png"></Image>
   `
 })
